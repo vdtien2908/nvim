e04cfd5cf851d072d51b51cef3ec9bdede33e3a4
vdtien2908 <vuductien2908@gmail.com>
2024-07-21 13:21:31 +0700

    fix plugin

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,12 +6,16 @@ vim.g.mapleader = " "
 vim.g.background = "light"
 
 vim.opt.swapfile = false
-vim.opt.cmdheight = 0 
+vim.opt.linespace = 2
+
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- save file
+--vim.keymap.set({ "i", "x", "n", "s" }, "<c-s>", ":wincmd w<CR>",{})
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
