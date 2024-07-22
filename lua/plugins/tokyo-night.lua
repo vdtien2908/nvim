076fdf24f601fec45c5cc6821d46dfcb6e7175a7
vdtien2908 <vuductien2908@gmail.com>
2024-07-22 22:10:58 +0700

    change bgr transparen, add keymap

--- lua/plugins/tokyo-night.lua
+++ lua/plugins/tokyo-night.lua
@@ -3,6 +3,15 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("tokyonight-storm")
+			require("tokyonight").setup({
+					style = "storm", -- hoặc "night" hoặc "day"
+					transparent = true, -- Bật nền trong suốt
+					terminal_colors = true, -- Áp dụng màu trong terminal Neovim
+					styles = {
+							sidebars = "transparent", -- Làm trong suốt các thanh bên
+							floats = "transparent", -- Làm trong suốt các cửa sổ nổi
+					},
+			})
+			vim.cmd.colorscheme("tokyonight-storm")
 	end,
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,22 +1,97 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-vim.g.background = "light"
 
-vim.opt.swapfile = false
-vim.opt.linespace = 2
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+vim.g.mapleader = " "
+
+-- Options -----------------------------------------------
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.number = true
+
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 3
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.shell = "fish"
+vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.wrap = false -- No Wrap lines
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+vim.opt.wildignore:append({ "*/node_modules/*" })
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
+vim.opt.splitkeep = "cursor"
+vim.opt.mouse = ""
+
+-- Keymap ------------------------------------------------
+
+-- Do things without affecting the registers
+keymap.set("n", "x", '"_x')
+keymap.set("n", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>P", '"0P')
+keymap.set("v", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>c", '"_c')
+keymap.set("n", "<Leader>C", '"_C')
+keymap.set("v", "<Leader>c", '"_c')
+keymap.set("v", "<Leader>C", '"_C')
+keymap.set("n", "<Leader>d", '"_d')
+keymap.set("n", "<Leader>D", '"_D')
+keymap.set("v", "<Leader>d", '"_d')
+keymap.set("v", "<Leader>D", '"_D')
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", 'vb"_d')
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+keymap.set("n", "<c-k>", ":wincmd k<CR>", opts)
+keymap.set("n", "<c-j>", ":wincmd j<CR>", opts)
+keymap.set("n", "<c-h>", ":wincmd h<CR>", opts)
+keymap.set("n", "<c-l>", ":wincmd l<CR>", opts)
 
--- save file
---vim.keymap.set({ "i", "x", "n", "s" }, "<c-s>", ":wincmd w<CR>",{})
+-- New tab
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.wo.number = true
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
 
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Modify files
+keymap.set("n", "<leader>q", ":q<CR>", opts)
+keymap.set("n", "<leader>Q", ":qa<CR>", opts)
+keymap.set("n", "<leader>w", ":w<CR>", opts)
