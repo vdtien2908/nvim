8262e2d08ce3ca0d05e8db94f198fd6d95694e35
vdtien2908 <vuductien2908@gmail.com>
2024-07-22 23:58:00 +0700

    fix change terminal cursor when quit nvim

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -29,12 +29,12 @@ vim.opt.smarttab = true
 vim.opt.breakindent = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
-vim.opt.wrap = false          -- No Wrap lines
+vim.opt.wrap = false -- No Wrap lines
 vim.opt.backspace = { "start", "eol", "indent" }
 vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
 vim.opt.wildignore:append({ "*/node_modules/*" })
-vim.opt.splitbelow = true     -- Put new windows below current
-vim.opt.splitright = true     -- Put new windows right of current
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
 vim.opt.splitkeep = "cursor"
 vim.opt.mouse = ""
 
@@ -95,3 +95,10 @@ keymap.set("n", "<C-w><down>", "<C-w>-")
 keymap.set("n", "<leader>q", ":q<CR>", opts)
 keymap.set("n", "<leader>Q", ":qa<CR>", opts)
 keymap.set("n", "<leader>w", ":w<CR>", opts)
+
+vim.cmd([[
+    augroup RestoreCursorShapeOnExit
+        autocmd!
+        autocmd VimLeave * set guicursor=a:ver1
+    augroup END
+]])
